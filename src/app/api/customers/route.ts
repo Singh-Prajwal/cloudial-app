25aaa2f41c10b670f39d40303a7d6beef9029e4e
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-27 17:47:17 +0530

    Path Updated

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -1,8 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../lib/prisma";
 import { getServerSession } from "next-auth";
-import { authOptions } from "../auth/[...nextauth]/route";
-
+// import { authOptions } from "../auth/[...nextauth]/route";
+import { authOptions } from "../../../lib/authOptions";
 export async function GET(req: NextRequest) {
   const session = await getServerSession(authOptions);
 
