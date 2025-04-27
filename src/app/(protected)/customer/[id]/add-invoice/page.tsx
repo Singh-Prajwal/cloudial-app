604c9cab3660c2119af59ce15e814f2e7a5a4dde
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-27 09:39:51 +0530

    Fix

--- src/app/(protected)/customer/[id]/add-invoice/page.tsx
+++ src/app/(protected)/customer/[id]/add-invoice/page.tsx
@@ -2,7 +2,6 @@
 import { JSX, useState } from "react";
 import { addInvoice } from "../../../../actions/addInvoice";
 import BackButton from "@/app/components/backButton";
-import { use } from "react";
 export default function AddInvoicePage({
   params,
 }: {
@@ -13,7 +12,6 @@ export default function AddInvoicePage({
   const [dueDate, setDueDate] = useState("");
   // const { id } = use(params);
   const { id } = params;
-  console.log("ID", id);
   return (
     <div className="max-w-md mx-auto text-black mt-10 p-6 bg-white rounded-2xl shadow-lg">
       <div className="flex justify-between items-center mb-4">
