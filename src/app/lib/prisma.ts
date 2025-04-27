3878db226a93f5b5d026d59ff0ea2072d6bea6cd
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-27 22:26:17 +0530

    Prisma

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -3,12 +3,7 @@ import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
 
-export const prisma = new PrismaClient({
-  log:
-    process.env.NODE_ENV === "development"
-      ? ["query", "error", "warn"]
-      : ["error"],
-});
+export const prisma = new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
