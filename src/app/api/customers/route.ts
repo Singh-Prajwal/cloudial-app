f39276920e9540a9afadcb6257f494288008a3f4
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-27 21:44:39 +0530

    Server Side Apis Fixed

--- src/app/api/customers/route.ts
+++ src/app/api/customers/route.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "../../lib/prisma";
 import { getServerSession } from "next-auth";
--- src/app/api/external/invoices/route.ts
+++ src/app/api/external/invoices/route.ts
@@ -1,3 +1,5 @@
+"use server";
+
 import prisma from "@/app/lib/prisma";
 
 export async function POST(req: Request) {
