b2b603f89d19069c96ac9186a39e22a9f3c8871a
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-29 17:51:28 +0530

    Prisma updated

--- prisma/generated/client/index.js
+++ prisma/generated/client/index.js
@@ -203,7 +203,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../..",
--- prisma/migrations/20250424034326_update_invoice_updated_at/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "Invoice" ALTER COLUMN "updatedAt" DROP NOT NULL;
