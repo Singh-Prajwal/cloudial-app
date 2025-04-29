059d2d35624992df0e019cb34e744d8e0cb1b981
Prajwal Singh <prajwal.singh.226@gmail.com>
2025-04-29 18:12:33 +0530

    Updated prisma lib and added vercel build script

--- src/app/lib/prisma.ts
+++ src/app/lib/prisma.ts
@@ -1,10 +1,26 @@
-// src/app/lib/prisma.ts
+// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 
-const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+let prisma: PrismaClient;
 
-const prisma = new PrismaClient();
-
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV === "production") {
+  prisma = new PrismaClient();
+} else {
+  // In development, use a global variable to prevent multiple instances
+  if (!(global as any).prisma) {
+    (global as any).prisma = new PrismaClient();
+  }
+  prisma = (global as any).prisma;
+}
 
 export default prisma;
+// // src/app/lib/prisma.ts
+// import { PrismaClient } from "@prisma/client";
+
+// const globalForPrisma = globalThis as unknown as { prisma: PrismaClient };
+
+// const prisma = new PrismaClient();
+
+// if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+
+// export default prisma;
